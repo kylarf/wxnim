4f8a7697747ae0c93df04fefa3bf0e72fb20da4a
kylarf <56093925+kylarf@users.noreply.github.com>
2021-12-16 23:41:53 -0800

    Corrected change made in previous commit to wx-config invocation made in previous commit from "wx-config libs --all" (incorrect) to "wx-config --libs all" (correct)

--- wxnim/wxCompile.nim
+++ wxnim/wxCompile.nim
@@ -95,9 +95,9 @@ elif defined(gcc) or defined(clang):
   else: # not defined(windows)
     const wxConfig =
       when defined(wxWidgetsPath):
-        wxWidgetsPath / "wx-config libs --all"
+        wxWidgetsPath / "wx-config --libs all"
       else:
-        "wx-config libs --all"
+        "wx-config --libs all"
     const wxConfigOptions {.strdefine.}: string = ""
     {.passC: "`" & wxConfig & " " & wxConfigOptions & " --cppflags`".}
     {.passL: "`" & wxConfig & " " & wxConfigOptions & " --libs`".}
